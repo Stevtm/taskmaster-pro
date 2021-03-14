42b1f2999d83476ce4f24bd522a83df74f8b2f65
Stevtm <steventmaddison@outlook.com>
2021-03-13 23:58:44 -0500

    add ability to change date

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,13 +42,18 @@ var saveTasks = function () {
 	localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// p element (task description) was clicked
 $(".list-group").on("click", "p", function () {
+	// get the current text
 	var text = $(this).text().trim();
 
+	// create a new textarea element
 	var textInput = $("<textarea>").addClass("form-control").val(text);
 
+	// swap out the elements
 	$(this).replaceWith(textInput);
 
+	// automatically focus on the element
 	textInput.trigger("focus");
 });
 
@@ -70,6 +75,48 @@ $(".list-group").on("blur", "textarea", function () {
 
 	// replace textarea with p element
 	$(this).replaceWith(taskP);
+});
+
+// span element (due date) was clicked
+$(".list-group").on("click", "span", function () {
+	// get the current text
+	var date = $(this).text().trim();
+
+	// create a new input element
+	var dateInput = $("<input>")
+		.attr("type", "text")
+		.addClass("form-control")
+		.val(date);
+
+	// swap out the elements
+	$(this).replaceWith(dateInput);
+
+	// automatically focus on the new element
+	dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function () {
+	// get the current text
+	var date = $(this).val().trim();
+
+	// get the parent ul's if attribute
+	var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+
+	// get the task's position in the list of other li elements
+	var index = $(this).closest(".list-group-item").index();
+
+	// update task in array and re-save to localStorage
+	tasks[status][index].date = date;
+	saveTasks();
+
+	// recreate the span element with bootstrap classes
+	var taskSpan = $("<span>")
+		.addClass("bage badge-primary badge-pill")
+		.text(date);
+
+	// replace input with span element
+	$(this).replaceWith(taskSpan);
 });
 
 // modal was triggered
